ac045e0689305e9ac94158e926a469fe6cbb8d5e
EC2 Default User <kyosuke001@gmail.com>
2016-05-30 05:05:37 +0000

    Webpage now successfully displays the data from the MySQL tables

--- server/website/index.php
+++ server/website/index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="refresh" content="5" />
 <title>NMP</title>
 <link href="css/styles.css" rel="stylesheet" type="text/css" />
 </head>
@@ -32,18 +33,29 @@
 		$password = "";
 		$hostname = "localhost";
 		$conn = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL"); 
-		echo "<br>Connected to MySQL<br>";
 		$select = mysql_select_db("cs184", $conn) or die("Could not select CS184");
-		echo "Selected CS184<br>";
 
-		$result = mysql_query("SELECT * FROM network_data");
-		while($row = mysql_fetch_array($result)){
-				echo '';
-				foreach($row as $field){
-						echo htmlspecialchars($field);
-				}
-				echo '<br>';
+		$result = mysql_query("SELECT * FROM network_data");	
+					
+		echo "<table border='2'> <tr> <th>admin_username</th> <th>computer_id</th> <th>computer_IP</th> <th>time_of_update</th> <th>computer_status</th>  <th>cpu_load</th>  <th>computer_temp</th>  <th>network_load</th>  <th>status_description</th> </tr>";
+
+		while($row = mysql_fetch_array($result))
+		{
+			echo "<tr>";
+			echo '<td>' . $row['admin_username'] . '</td>';
+			echo "<td>" . $row['computer_id'] . "</td>";
+			echo "<td>" . $row['computer_IP'] . "</td>";
+			echo "<td>" . $row['time_of_update'] . "</td>";	
+			echo "<td>" . $row['computer_status'] . "</td>";
+			echo "<td>" . $row['cpu_load'] . "</td>";
+			echo "<td>" . $row['computer_temp'] . "</td>";
+			echo "<td>" . $row['network_load'] . "</td>";
+			echo "<td>" . $row['status_description'] . "</td>";
+			echo "</tr>";
+			
 		}
+		echo "</table>";
+	
 		mysql_free_result($result); 
 		mysql_close($conn);
 	?> 
