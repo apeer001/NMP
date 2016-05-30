db1f88ab67cc0769528a2e2a3904bfcd8906041b
apeer001 <apeer001@ucr.edu>
2016-05-29 19:49:34 -0700

    updated installwebsite

--- server/website/installwebsite.sh
+++ server/website/installwebsite.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 sudo cp ./index.html /var/www/html/
-sudo cp ./dataDump.php /var/www/html/
-sudo cp ./phpinfo.php /var/www/html/
-sudo cp ./addUser.php /var/www/html/
+sudo cp ./about,html /var/www/html/
+sudo cp ./contact.html /var/www/html/
+sudo cp ./services.html /var/www/html/
+sudo cp ./works.html /var/www/html/
+sudo cp -r ./css/ /var/www/html/
+sudo cp -r ./images /var/www/html/
