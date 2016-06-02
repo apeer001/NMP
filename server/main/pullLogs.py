c799e6ab7e13bf3f0e3738e8cff7479a872f38d6
apeer001 <apeer001@ucr.edu>
2016-06-02 09:19:05 -0700

    updated pull Logs to check for most recent error logs

--- server/main/pullLogs.py
+++ server/main/pullLogs.py
@@ -2,6 +2,7 @@
 # Author: Aaron Peery
 
 from datetime import datetime
+from datetime import timedelta
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import httplib
 import sys
@@ -46,7 +47,7 @@ def getLogFromClient(ip):
 
     return data_received
 
-def updateLogDB(cur,ip,comp_id,logpart,user='root'):
+def updateLogDB(cur,ip,comp_id,logpart,user='root',table=table_data):
     print('INSERTING NEW LOG')   
     if len(logpart) ==  6:                                                  
         timestamp = logpart[0]
@@ -56,17 +57,19 @@ def updateLogDB(cur,ip,comp_id,logpart,user='root'):
         net_load = logpart[4]
         description = logpart[5]
     else:
-        timestamp = '2016-05-29 03:14:07'
-        comp_status = 'OK'
-        cpu_load = 4
-        comp_temp = 10
-        net_load = 20
+    	d = datatime.datetime.now()
+    	d.strftime("YY-MM-DD HH:mm:ss")
+        timestamp = d
+        comp_status = 'CRITICAL'
+        cpu_load = -1
+        comp_temp = -1
+        net_load = -1
         description = 'default log entry for testing'        
     
     # Insert data into mysql database
     # Data Insert into the table
     insert_stmt = (
-        "INSERT INTO " + table_data + " (admin_username, computer_id, computer_IP, time_of_update, computer_status, cpu_load, computer_temp, network_load, status_description) "
+        "INSERT INTO " + table + " (admin_username, computer_id, computer_IP, time_of_update, computer_status, cpu_load, computer_temp, network_load, status_description) "
         "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
     )
     data = (user, comp_id, ip, timestamp, comp_status, cpu_load, comp_temp, net_load, description)
@@ -109,15 +112,54 @@ def getLogs():
             else:
                 d = datetime.datetime.now()
                 d.strftime("YY-MM-DD HH:mm:ss") 
-                logPart = ['','OFFLINE',-1,-1,-1,'This client is unable to be reached. Needs Attention!']
+                logPart = [d,'OFFLINE',-1,-1,-1,'This client is unable to be reached. Needs Attention!']
                 updateLogDB(cur,comp_ip,comp_id,logPart,admin_user)
         except:
             print('Unable to connect to ' + comp_ip)
     
     db.close()
     return 0
+
+
+# Checks the 'network_data' table and finds all logs from the last 10 minutes
+# and places them in the 'most_recent_network_status' table
+ def updateMostRecentErrorsDB():
+
+ 	print('updating ' +  table_recent + ' requests is starting...')
+    
+    db = MySQLdb.connect(host=hostname,     # your host, usually localhost
+                         user=username,     # username
+                         passwd=password,   # password
+                         db=database)       # name of the database
+
+    # must create a cursor object
+    cur = db.cursor()
+    
+    # Query for all ip addresses in network table
+    upperBound = datetime.datetime.now()
+    lowerBound = upperBound - datetime.timedelta(minutes=10)
+    upperBound.strftime("YY-MM-DD HH:mm:ss") 
+    lowerBound.strftime("YY-MM-DD HH:mm:ss") 
+    print(lowerBound)
+    print(upperBound)
+    query_stmt = "SELECT * FROM " + table_data + " WHERE time_of_update BETWEEN '" + lowerBound +"' '" + upperBound + "'"
+    cur.execute(query_stmt)  
+    for (admin_user, comp_id, comp_ip, timestamp, comp_status, cpu_load, comp_temp, net_load, description) in cur.fetchall():
+        print(admin_user + " " + str(comp_id) + " " + str(comp_ip))
+        # update log row
+        try:
+			for line in lines:
+			logPart = [timestamp, comp_status, cpu_load, comp_temp, net_load, description]
+			updateLogDB(cur,comp_ip,comp_id,logPart,admin_user,table_recent)
+			db.commit()         
+        except:
+            db.rollback()
+            print('Rolling back database due to error')
+            
+    db.close()
  
 # Main
 if __name__ == '__main__':
     getLogs()
+    updateMostRecentErrorsDB()
 
